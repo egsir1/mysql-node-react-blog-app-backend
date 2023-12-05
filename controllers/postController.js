b437da257206ad5178b9b84cfeb726d5f7413c23
egsir1 <egsir111@gmail.com>
2023-12-05 15:11:52 +0900

    feat: Create & update post rest api business logic

--- controllers/postController.js
+++ controllers/postController.js
@@ -26,11 +26,38 @@ export const getPost = (req, res) => {
   });
 };
 
+// create post
+
 export const addPost = (req, res) => {
-  res.json("from controller");
+  console.log("creat post");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not Authenticated");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid");
+
+    const q =
+      "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`, `uid`) VALUES (?)";
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+      req.body.date,
+      userInfo.id,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+
+      return res.json("Post has been created.");
+    });
+  });
 };
 
 export const deletePost = (req, res) => {
+  console.log("delete post");
   const token = req.cookies.access_token;
 
   if (!token) return res.status(401).json("Not Authenticated");
@@ -49,6 +76,25 @@ export const deletePost = (req, res) => {
   });
 };
 
+// update post
+
 export const updatePost = (req, res) => {
-  res.json("from controller");
+  console.log("update post");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not authenticated!");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const postId = req.params.id;
+    const q =
+      "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
+
+    const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+
+    db.query(q, [...values, postId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("Post has been updated.");
+    });
+  });
 };
--- server.js
+++ server.js
@@ -2,13 +2,29 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
-
+import multer from "multer";
 const app = express();
 import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import usersPosts from "./routes/users.js";
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req?.file;
+  res.status(200).json(file?.filename);
+});
 
 app.use("/api/posts", postRoutes);
 app.use("/api/users", usersPosts);
