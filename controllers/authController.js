cc845371e6274c6807f134103182b8e562258ebf
egsir1 <egsir111@gmail.com>
2023-12-05 10:31:54 +0900

    feat: Login & Logout with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { db } from "../db.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
+//sign up
 export const register = (req, res) => {
   //CHECK EXISTING USER
   console.log(req.body);
@@ -24,10 +25,43 @@ export const register = (req, res) => {
   });
 };
 
+//login
 export const login = (req, res) => {
-  res.json("from controller");
+  //CHECK USER
+
+  const q = "SELECT * FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    //Check password
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password");
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  });
 };
 
 export const logout = (req, res) => {
-  res.json("from controller");
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .json("User has been logged out");
 };
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 
 const app = express();
 import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import usersPosts from "./routes/users.js";
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts", postRoutes);
 app.use("/api/users", usersPosts);
